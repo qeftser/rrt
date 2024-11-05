fba1ca78358e85f04b726cf8e3ec4f7237f8bce7
qeftser <qeftser1@gmail.com>
2024-11-05 14:19:44 -0700

    binary heap. needs fixing

---
+++ src/binary_heap.hpp
@@ -0,0 +1,128 @@
+
+#ifndef __BINARY_HEAP
+
+#define __BINARY_HEAP
+#include <vector>
+#include <cstdint>
+#include <cstdlib>
+#include <unordered_map>
+#include <cstdio>
+
+template<typename V>
+class binary_heap {
+public:
+
+   binary_heap() {
+      elements = std::vector<pair *>();
+      members = std::unordered_map<uint64_t,pair *>();
+      total = 0;
+   }
+
+   ~binary_heap() {
+   }
+
+   void insert(uint64_t key, V val) {
+      struct pair * nev = (struct pair *)malloc(sizeof(struct pair));
+      nev->key = key; nev->pos = total; nev->val = val;
+      elements.push_back(nev);
+      //members[elements[total]->key] = elements[total];
+      up_heap(total);
+      ++total;
+   }
+
+   V extract(void) {
+      //printf("total: %d\n",total);
+      --total;
+      temp = elements[0];
+      elements[0] = elements[total];
+      elements[total] = NULL;
+      elements.pop_back();
+      //members.erase(temp->key);
+      down_heap(0);
+      V ret = temp->val;
+      //printf("%p\n",temp);
+      //free(temp);
+      temp = NULL;
+      return ret;
+   }
+
+   void update_key(uint64_t old, uint64_t nev) {
+      struct pair * curr = members.at(old);
+      curr->key = nev;
+      if (old > nev)
+         up_heap(curr->pos);
+      else
+         down_heap(curr->pos);
+   }
+
+   bool contains(uint64_t key) {
+      return members.count(key);
+   }
+
+   bool empty() {
+      return !total;
+   }
+
+private:
+
+   struct pair {
+      uint64_t key;
+      int pos;
+      V val;
+   };
+
+   std::vector<pair *> elements;
+   std::unordered_map<uint64_t,pair *> members;
+   struct pair * temp;
+   int total;
+
+   inline const int left(const int i) const {
+      return (i+i)+1;
+   }
+
+   inline const int right(const int i) const {
+      return (i+i)+2;
+   }
+
+   inline const int parent(const int i) const {
+      return (i-1)>>1;
+   }
+
+   inline const void up_heap(const int i) {
+      if (!i)
+         return;
+      if (elements[i]->key < elements[parent(i)]->key) {
+         temp = elements[i];
+         elements[i] = elements[parent(i)];
+         elements[parent(i)] = temp;
+         elements[parent(i)]->pos = elements[i]->pos;
+         elements[i]->pos = temp->pos;
+         up_heap(parent(i));
+      }
+   }
+
+   inline const void down_heap(const int i) {
+      if (left(i)-1 >= total || right(i) >= total)
+         return;
+      if (elements[left(i)] < elements[i]) {
+         if (elements[right(i)] < elements[left(i)]) {
+            temp = elements[right(i)];
+            elements[right(i)] = elements[i];
+            elements[i] = temp;
+            elements[i]->pos = elements[right(i)]->pos;
+            elements[right(i)]->pos = temp->pos;
+            down_heap(right(i));
+         }
+         else {
+            temp = elements[left(i)];
+            elements[left(i)] = elements[i];
+            elements[i] = temp;
+            elements[i]->pos = elements[left(i)]->pos;
+            elements[left(i)]->pos = temp->pos;
+            down_heap(left(i));
+         }
+      }
+   }
+};
+
+#endif
--- src/scratch.cpp
+++ src/scratch.cpp
@@ -1,5 +1,6 @@
 
 #include "fibonacci_heap.hpp"
+#include "binary_heap.hpp"
 #include <queue>
 #include <ctime>
 
@@ -11,12 +12,15 @@ int main(void) {
 
    fibonacci_heap<int,int,std::less<int>> F;
    std::priority_queue<int> Q;
+   binary_heap<int> B;
 
    srand(time(NULL)*clock());
 
+   int total = 0x7fffff;
+
    {
       clock_t tStart = clock();
-      for (int i = 0; i < 0x1ffffff; ++i) {
+      for (int i = 0; i < total; ++i) {
          int val = rand();
          F.insert(val,val);
       }
@@ -24,13 +28,20 @@ int main(void) {
    }
    {
       clock_t tStart = clock();
-      for (int i = 0; i < 0x1ffffff; ++i) {
+      for (int i = 0; i < total; ++i) {
          int val = rand();
          Q.push(val);
       }
       printf("priority_queue load: %f\n",(double)(clock()-tStart)/CLOCKS_PER_SEC);
    }
-
+   {
+      clock_t tStart = clock();
+      for (int i = 0; i < total; ++i) {
+         int val = rand();
+         B.insert(val,val);
+      }
+      printf("binary load: %f\n",(double)(clock()-tStart)/CLOCKS_PER_SEC);
+   }
 
    {
       clock_t tStart = clock();
@@ -39,13 +50,19 @@ int main(void) {
       }
       printf("fibonacci dump: %f\n",(double)(clock()-tStart)/CLOCKS_PER_SEC);
    }
-   
    {
       clock_t tStart = clock();
       while (!Q.empty()) {
          Q.pop();
       }
       printf("priority_queue dump: %f\n",(double)(clock()-tStart)/CLOCKS_PER_SEC);
+   }
+   {
+      clock_t tStart = clock();
+      while (!B.empty()) {
+         B.extract();
+      }
+      printf("binary dump: %f\n",(double)(clock()-tStart)/CLOCKS_PER_SEC);
    }
 
    return 0;
